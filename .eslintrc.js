c9f4181c1fae9a4a903b219cbf29339a16b7ba09
voidpumpkin <32368314+voidpumpkin@users.noreply.github.com>
2019-09-17 22:03:36 +0300

    config prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
-let prettierRules = require("./prettier.config.js");
+let prettierRules = require('./prettier.config.js');
 module.exports = {
-        extends: ["prettier"],
-        plugins: ["prettier"],
-        rules: {
-                "prettier/prettier": ["error", prettierRules]
-        }
+    extends: ['prettier'],
+    plugins: ['prettier'],
+    rules: {
+        'prettier/prettier': ['error', prettierRules]
+    }
 };
